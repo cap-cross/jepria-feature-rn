7e8f6d79aab5205febd0256018641a9f70de5d96
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-10-09 10:29:29 +0300

    refacto: удаление лишнего

--- app/src/api/apiConfig.js
+++ app/src/api/apiConfig.js
@@ -5,10 +5,8 @@
 const FEATURE_SERVICE_HOST = '10.0.3.2'; // localhost
 const FEATURE_SERVICE_PORT = '8080';
 
-// const featureServiceContext = 'feature-json-jepria-backend-nagornyys';
 export const FEATURE_SERVICE_CONTEXT = 'feature-json-jepria-backend';
 const API_VERSION = 'v1';
-// export const FEATURE_SERVICE_CONTEXT = 'feature-json-jepria-backend';
 export const FEATURE_API = 'features';
 
 export const BASE_URL = `http://${FEATURE_SERVICE_HOST}:${FEATURE_SERVICE_PORT}`;
@@ -21,4 +19,3 @@ export const FEATURE_API_UPDATE_URL = FEATURE_API_URL;
 export const FEATURE_API_DELETE_URL = FEATURE_API_URL;
 export const FEATURE_STATUSES_URL = `${FEATURE_CONTEXT_URL}/featurestatuses`;
 export const FEATURE_OPERATORS_URL = `${FEATURE_CONTEXT_URL}/featureoperators`;
-
