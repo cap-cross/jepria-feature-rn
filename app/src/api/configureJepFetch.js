4e7a564a394965b0eba4f97f067ce70988c29336
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-11-02 17:17:25 +0300

    refactor: удаление лишнего

--- app/src/api/configureJepFetch.js
+++ app/src/api/configureJepFetch.js
@@ -4,13 +4,8 @@ import log from '@cap-cross/cap-core';
 
 function configureJepFetch(loginApi) {
   const {secureFetch, authenticate, shouldAuthenticate} = loginApi;
-  log.trace("configureJepFetch(): loginApi = " + JSON.stringify(loginApi));
-  log.trace("configureJepFetch(): secureFetchPromise = " + secureFetch);
-  log.trace("configureJepFetch(): authenticate = " + authenticate);
-  log.trace("configureJepFetch(): shouldAuthenticate = " + shouldAuthenticate);
 
   return async (input, init) => {
-    log.trace("configureJepFetch(): secureFetch = " + secureFetch);
     return await secureFetch(input, init)
       .catch((error) => {
         if (shouldAuthenticate(error)) {
